dc09789ebe0691be2a8d4d6723e33bf4976bd6da
Lucas Rodrigues <Lucasrodrigues4@live.com>
2026-07-17 11:17:15 -0300

    -

--- src/config/admins.js
+++ src/config/admins.js
@@ -1,5 +1,5 @@
 module.exports = [
-  { username: "lucas", password: "1234" },
+  { username: "teste", password: "1234" },
   { username: "Paulo", password: "2026" },
    { username: "Andre", password: "2026" },
 ];
